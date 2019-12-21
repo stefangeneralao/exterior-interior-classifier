67992744ae2ff42fc1138e965be0cff6ae36971c
Stefan Generalao <stefan.generalao@gmail.com>
2019-12-21 04:19:38 +0000

    Updated docker implementation

--- client/update_docker_image.sh
+++ client/update_docker_image.sh
@@ -3,9 +3,9 @@ if [[ $EUID -ne 0 ]]; then
   echo "This script must be run as root" 
   exit 1
 fi
+yarn
 yarn build
 docker build -t extint_client .
-rm -rf build
 docker stop extint_client || true
 docker rm extint_client || true
 docker run -p 3000:3000 -d --name extint_client extint_client
